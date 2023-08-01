bca5f861ad2c7cbe8a62dc751498919d61697bdc
Luiz Rocco <luizgustavorocco@gmail.com>
2023-08-01 01:48:52 -0300

    Testing dynamic colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,20 @@
+import { useState } from "react";
+
 export const App = () => {
-  return <h1 className="text-2xl font-bold underline">Hello world!</h1>;
+  const [color, setColor] = useState("bg-[#4287f5]");
+
+  return (
+    <div>
+      <h1 className={`border-4 ${color}`}>Hello world!</h1>
+      <button
+        onClick={() =>
+          setColor((prevColor) =>
+            prevColor === "bg-[#4287f5]" ? "bg-[#f50000]" : "bg-[#4287f5]"
+          )
+        }
+      >
+        Change color!
+      </button>
+    </div>
+  );
 };
